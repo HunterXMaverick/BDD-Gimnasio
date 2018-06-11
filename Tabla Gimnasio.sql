47a0bab951ff4fbc3f2e7a0a74080d9df64bd5f7
HunterXMaverick <jfo.silva@yavirac.edu.ec>
2018-06-11 01:24:39 -0500

    modifcacion de tabla

--- Tabla Gimnasio.sql
+++ Tabla Gimnasio.sql
@@ -1,3 +1,15 @@
+
+Drop table if exists Precio;
+
+select * from Persona;
+select * from Empleado;
+select * from Cliente;
+select * from MedidaPersonal;
+select * from Precio;
+select * from TipoEntrenamiento;
+
+
+
 Create database Gimnasio;
 
 Use Gimnasio;
@@ -10,55 +22,54 @@ cedula varchar (10) not null,
 telefono varchar (10) not null,
 PRIMARY KEY (id));
 
-insert into Persona Values (1, 'Juan', 'Silva', '1717833451', '0989122481');
 
 Create table Empleado (
 id int auto_increment,
 nutrisionista varchar(30) not null,
-idtipoEntrenamiento int not null,
-idPersona int not null,
+-- idtipoEntrenamiento int not null,
+-- idPersona int not null,
 PRIMARY KEY (id));
 
-insert into Empleado Values (1, 'Alfonso');
 
-Create table cliente(
+Create table Cliente(
 id int auto_increment,
 idmedidaPersona int not null,
 idPersona int not null,
 PRIMARY KEY (id));
 
-insert into Cliente Values (1, 'definición y aumento muscular');
 
-
-Create table MedidaPersona (
+Create table MedidaPersonal (
 id int auto_increment,
-pecho varchar(5) not null,
-brazo varchar(5) not null,
-cintura varchar(5) not null,
-cadera varchar(5) not null,
-hombros varchar(5) not null,
-piernas varchar(5) not null,
-pantorrillas varchar(5) not null,
-idcliente int not null,
+pecho varchar(10) not null,
+brazo varchar(10) not null,
+cintura varchar(10) not null,
+cadera varchar(10) not null,
+hombros varchar(10) not null,
+piernas varchar(10) not null,
+pantorrillas varchar(10) not null,
+-- idcliente int not null,
 PRIMARY KEY (id));
 
-insert into MedidaPersona Values (1, '105', '45', '89', '65', '125', '136', '145');
+insert into MedidaPersonal (pecho, brazo, cintura, cadera, hombros, piernas, pantorrillas) Values ('105', '45', '89', '65', '125', '136', '145');
 
 
 Create table Precio(
 id int auto_increment,
-total varchar(10) not null,
-idPersona int not null,
+cantidad varchar(10) not null,
+-- idPersona int not null,
 PRIMARY KEY (id));
 
-insert into Precio Values (1, '24.50$ mensual');
+insert into Precio (cantidad) Values ('24$ mensual');
 
 Create table TipoEntrenamiento(
 id int auto_increment,
 detalle varchar(100) not null,
 PRIMARY KEY (id));
 
-insert into TipoEntrenamiento Values (1, 'definición y aumento muscular');
+insert into Persona (nombre, apellido, cedula, telefono) Values ('Juan', 'Silva', '1717833451', '0989122481');
+insert into Empleado (nutrisionista) Values ('Alfonso');
+insert into Cliente Values (1, 'definición y aumento muscular');
+insert into MedidaPersona Values (1, '105', '45', '89', '65', '125', '136', '145');
+insert into TipoEntrenamiento (detalle) Values ('definición y aumento muscular');
 
-Drop table if exists Precio
 
